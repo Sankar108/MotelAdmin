d32cba16b561bb1d0bb47143087c5d2819158986
Sankar108 <gsahu447@gmail.com>
2020-12-01 22:24:22 +0530

    register component added

--- src/app/pages/layout/layout.module.ts
+++ src/app/pages/layout/layout.module.ts
@@ -7,9 +7,10 @@ import { CheckinComponent } from './customer/checkin/checkin.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HeaderComponent } from 'src/app/shared/components/header/header.component';
 import { RoomsComponent } from './rooms/rooms.component';
+import { RegisterComponent } from './customer/register/register.component';
 
 @NgModule({
-  declarations: [DashboardComponent, RoomInfoComponent, CheckinComponent, RoomsComponent],
+  declarations: [DashboardComponent, RoomInfoComponent, CheckinComponent, RoomsComponent,RegisterComponent],
   imports: [
     CommonModule,
     LayoutRoutingModule,
