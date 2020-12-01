c77dba12fd9df7bd9a9ce5af73995fcb75f95774
Sankar108 <gsahu447@gmail.com>
2020-12-01 22:45:11 +0530

    room info moved

--- src/app/pages/layout/layout.module.ts
+++ src/app/pages/layout/layout.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LayoutRoutingModule } from './layout-routing.module';
-import { RoomInfoComponent } from './room-info/room-info.component';
 import { CheckinComponent } from './customer/checkin/checkin.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HeaderComponent } from 'src/app/shared/components/header/header.component';
 import { RoomsComponent } from './rooms/rooms.component';
 import { RegisterComponent } from './customer/register/register.component';
+import { RoomInfoComponent } from './rooms/room-info/room-info.component';
 
 @NgModule({
   declarations: [DashboardComponent, RoomInfoComponent, CheckinComponent, RoomsComponent,RegisterComponent],
