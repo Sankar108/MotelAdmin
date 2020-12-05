138cc7b89a07a85e2702cb7c8faa2c52abff8239
Sankar108 <gsahu447@gmail.com>
2020-12-06 01:14:55 +0530

    dashboard updated

--- src/app/pages/layout/dashboard/dashboard.component.ts
+++ src/app/pages/layout/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { RoomModel } from 'src/app/models/room';
+import { RoomService } from 'src/app/services/room.service';
 import { UtilityService } from 'src/app/services/utility.service';
 
 @Component({
@@ -7,14 +10,36 @@ import { UtilityService } from 'src/app/services/utility.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  allRooms: RoomModel[] = [];
+  occupiedRooms: RoomModel[] = [];
+  vacantRooms: RoomModel[] = [];
+  undercleanRooms: RoomModel[] = [];
 
   constructor(
-    public utilityService: UtilityService
+    public utilityService: UtilityService,
+    private roomService: RoomService
   ) {
     this.utilityService.title = "Dashboard";
   }
 
   ngOnInit(): void {
+    this.GetRooms();
   }
 
+  GetRooms() {
+    this.utilityService.showLoader();
+    this.roomService.GetRooms().subscribe((response: any) => {
+      this.utilityService.hideLoader();
+      if (response.Succeeded) {
+        this.allRooms = response.Data;
+        this.occupiedRooms = this.allRooms.filter(r => r.IsOccupied === true && r.IsCleaned);
+        this.vacantRooms = this.allRooms.filter(r => r.IsOccupied === false && r.IsCleaned);
+        this.undercleanRooms = this.allRooms.filter(r => !r.IsOccupied === false && !r.IsCleaned);
+      }
+    },
+      (err: HttpErrorResponse) => {
+        this.utilityService.hideLoader();
+        this.utilityService.ShowMsg(err.message, this.utilityService.error);
+      })
+  }
 }
